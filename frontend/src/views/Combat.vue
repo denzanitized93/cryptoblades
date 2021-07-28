b672b662d5e8b426c85cbba0cc82a86895d938dd
denzanitized93 <denzanitized93@gmail.com>
2021-07-28 23:16:51 +0800

    Fix for #454
    Added v-show binding with durability checker.

--- frontend/src/views/Combat.vue
+++ frontend/src/views/Combat.vue
@@ -102,6 +102,7 @@
                     class="encounter-button btn-styled"
                     :mainText="`Fight!`"
                     v-tooltip="'Cost 40 stamina'"
+                    v-show="getWeaponDurability(selectedWeaponId) > 0"
                     :disabled="(timeMinutes === 59 && timeSeconds >= 30) || waitingResults"
                     @click="onClickEncounter(e)"
                   />
